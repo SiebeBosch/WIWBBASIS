ece5612364e0a85f9388c33133fc5a5862891ffc
Siebe Bosch <siebe@hydroconsult.nl>
2022-06-21 23:22:15 +0100

    initial commit

---
+++ WIWBBASIS/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WIWBBASIS")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WIWBBASIS")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7960d4d1-06c9-48af-85a0-c6fe8d1345ea")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.1.0.8")>
+<Assembly: AssemblyFileVersion("2.1.0.8")>
---
+++ WIWBBASIS/WIWBBASIS.vb
@@ -0,0 +1,117 @@
+﻿Option Explicit On
+
+Imports METEOBAS
+Imports System
+Imports Newtonsoft.Json.Linq
+
+Module WIWBBASIS
+
+    'Copyright Siebe Bosch Hydroconsult, 2012
+    'Lulofsstraat 55, unit 47 Den Haag, The Netherlands
+    'this program extracts precipitation and/or evaporation data from the WIWB API
+
+    'lokale variabelen
+    Dim Setup As New METEOBAS.General.clsSetup
+    Dim BasisData As New METEOBAS.clsWIWBBasisData(Setup)
+    Dim RunOnServer As Boolean = True
+
+    Sub Main()
+        Try
+            Dim myArg As String
+            Console.WriteLine("This program reads precipitation and evaporation data from the WIWB API")
+
+            '----------------------------------------------------------------------------------------------------------------------------
+            'system-dependent variables
+            '----------------------------------------------------------------------------------------------------------------------------
+            If RunOnServer Then
+                BasisData.DownloadURL = "https://www.meteobase.nl/meteobase/downloads/"                                                  'laptop en server
+                BasisData.DownloadDIR = "c:\Program Files (x86)\PostgreSQL\EnterpriseDB-ApachePHP\apache\www\meteobase\downloads\"  'server
+            Else
+                BasisData.DownloadURL = "https://www.meteobase.nl/meteobase/downloads/"                                                  'laptop en server
+                BasisData.DownloadDIR = "c:\temp\"  'local
+            End If
+
+            If My.Application.CommandLineArgs.Count = 0 Then
+                Console.WriteLine("Enter start date (YYYYMMDD)")
+                myArg = Console.ReadLine
+                BasisData.FDate = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Enter end date (YYYYMMDD)")
+                myArg = Console.ReadLine
+                BasisData.TDate = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Daily sum stations? (TRUE/FALSE)")
+                myArg = Console.ReadLine
+                BasisData.Etmaal = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Export precipitation? (TRUE/FALSE)")
+                myArg = Console.ReadLine
+                BasisData.NSL = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Export Makkink evaporation? (TRUE/FALSE)")
+                myArg = Console.ReadLine
+                BasisData.MAKKINK = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Enter the session ID")
+                myArg = Console.ReadLine
+                BasisData.SessionID = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Enter the order number")
+                myArg = Console.ReadLine
+                BasisData.OrderNum = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Enter the name of the person who orders")
+                myArg = Console.ReadLine
+                BasisData.Naam = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Enter their e-mailaddress")
+                myArg = Console.ReadLine
+                BasisData.MailAdres = myArg
+                Setup.Log.CmdArgs.Add(myArg)
+                Console.WriteLine("Enter the station number")
+                myArg = Console.ReadLine
+                BasisData.GetAddStationByNumber(myArg)
+                Setup.Log.CmdArgs.Add(myArg)
+
+            ElseIf My.Application.CommandLineArgs.Count < 10 Then
+                Console.WriteLine("Error: incorrect number of arguments presented")
+            Else
+                BasisData.FDate = My.Application.CommandLineArgs(0)
+                BasisData.TDate = My.Application.CommandLineArgs(1)
+                BasisData.Etmaal = Setup.GeneralFunctions.GetBooleanFromString(My.Application.CommandLineArgs(2))
+                BasisData.NSL = Setup.GeneralFunctions.GetBooleanFromString(My.Application.CommandLineArgs(3))
+                BasisData.MAKKINK = Setup.GeneralFunctions.GetBooleanFromString(My.Application.CommandLineArgs(4))
+                BasisData.SessionID = My.Application.CommandLineArgs(5)
+                BasisData.OrderNum = My.Application.CommandLineArgs(6)
+                BasisData.Naam = Setup.GeneralFunctions.RemoveBoundingQuotes(My.Application.CommandLineArgs(7))
+                BasisData.MailAdres = Setup.GeneralFunctions.RemoveBoundingQuotes(My.Application.CommandLineArgs(8))
+                For i = 9 To My.Application.CommandLineArgs.Count - 1
+                    BasisData.GetAddStationByNumber(My.Application.CommandLineArgs(i))
+                Next
+
+                For i = 0 To My.Application.CommandLineArgs.Count - 1
+                    Setup.Log.CmdArgs.Add(My.Application.CommandLineArgs(i))
+                Next
+
+            End If
+
+            '----------------------------------------------------------------------------------------------------------------------------
+            'query the API and start writing the data
+            '----------------------------------------------------------------------------------------------------------------------------
+            If BasisData.Build() Then  'write the requested datafiles
+                Call BasisData.InitializeGoodMail("Basisgegevens Meteobase")
+                Call BasisData.sendGoodEmail()
+            Else
+                Call BasisData.InitializeBadMail("Basisgegevens Meteobase")
+                Call BasisData.sendBadEmail()
+            End If
+        Catch ex As Exception
+            Setup.Log.AddError(ex.Message)
+            Call BasisData.InitializeBadMail("Basisgegevens Meteobase")
+            Call BasisData.sendBadEmail()
+        Finally
+            Setup.Log.write(BasisData.DownloadDIR & "\" & BasisData.SessionID & "_" & BasisData.OrderNum & ".log", False)
+        End Try
+
+    End Sub
+End Module
